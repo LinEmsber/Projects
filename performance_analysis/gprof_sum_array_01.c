887869d3701e2b7eaf0405efd61a34f77a4cf46b
Lin Emsber <linemsber@gmail.com>
2017-02-28 11:19:52 +0800

    write clear explanation, and add references

--- performance_analysis/gprof_sum_array_01.c
+++ performance_analysis/gprof_sum_array_01.c
@@ -1,27 +1,39 @@
-/* An implementation of array summing using Gprof to do a analysis. */
+/*
 
-/* Usage and explanation
+Using Gprof to conduct an analysis of array summation.
+We use different summation algorithms to compare the resources use.
 
-1. compile it, remeber to add option "-pg"
-> gcc -Wall -pg gprof_sum_array_01.c
+Gprof usage and explanation:
 
-2. MUST to run once to create a file, gmont.out, for analyze
+1. compile it, remember to add option "-pg" for gporf, and "-O0" for default optimization for
+compilation time.
+> gcc -O0 -Wall -pg gprof_sum_array_01.c
+
+2. After we compile the output file, we MUST to run this program once to create a file, gmont.out,
+for analysis.
 > ./a.out
 
-3. run Gprof to analyze your result
+3. run Gprof to analyze the result.
 > gprof -b ./a.out gmon.out
 
 COMBO:
-> gcc -Wall -pg gprof_sum_array_01.c && ./a.out && gprof -b ./a.out gmon.out
+> gcc -O0 -Wall -pg gprof_sum_array_01.c && ./a.out && gprof -b ./a.out gmon.out
 
 
 TODO:
-1. understand what is assert().
-2. try to do a Makefile for this project.
-3. use faster method (better algorithms) to do array summing.
-4. multi-thread
+use faster method (better algorithms) to do array summing.
+multi-thread
+
+
+References:
+http://os.51cto.com/art/200703/41426.htm
+https://www.ptt.cc/bbs/C_and_CPP/M.1246071002.A.A54.html
+https://en.wikipedia.org/wiki/Duff's_device
+http://www.rapidtables.com/code/linux/gcc/gcc-o.htm
 
 */
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,12 +42,16 @@ TODO:
 
 #include <assert.h>
 
-#define RAND_NUMS 100000
-#define RUN_TIMES 100000
+// the length of array
+#define RAND_NUMS 1000000
+
+// how many times to add array
+#define RUN_TIMES 100
 
 // global variables
-int sum_a = 0;
-int sum_b = 0;
+int sum_simplest = 0;
+int sum_reduces_brances = 0;
+int sum_duffs_device = 0;
 
 /* generate a random number array in a specific range
  *
@@ -45,23 +61,23 @@ int sum_b = 0;
  */
 int * rn_arr_gen( int len , int start, int end)
 {
-        int i;
-        int * ret_arr = NULL;
-        int range = start - end;
+	int i;
+	int * ret_arr = NULL;
+	int range = start - end;
 
-        // memroy allocation
-        ret_arr = (int *) malloc( len * sizeof(int) );
-        memset(ret_arr, 0, len + 1);
+	// memroy allocation
+	ret_arr = (int *) malloc( len * sizeof(int) );
+	memset(ret_arr, 0, len + 1);
 
-        // rand seed use time
-        srand( (unsigned) time(NULL) );
+	// rand seed use time
+	srand( (unsigned) time(NULL) );
 
-        // generate arr
-        for ( i = 0; i < len; i++ ){
-                *(ret_arr + i) = (rand() % range) + 1;
-        }
+	// generate arr
+	for ( i = 0; i < len; i++ ){
+		*(ret_arr + i) = (rand() % range) + 1;
+	}
 
-        return ret_arr;
+	return ret_arr;
 }
 
 /* print the array with specific lenght
@@ -71,12 +87,12 @@ int * rn_arr_gen( int len , int start, int end)
  */
 void print_arr(int *arr, int len)
 {
-        int i;
+	int i;
 
-        for ( i = 0; i < len; i++){
-                printf("%d ", *(arr + i) );
-        }
-        printf("\n");
+	for ( i = 0; i < len; i++){
+		printf("%d ", *(arr + i) );
+	}
+	printf("\n");
 }
 
 
@@ -84,51 +100,105 @@ void print_arr(int *arr, int len)
 int sum_array_simplest(int * array)
 {
 	int i;
-        for ( i = 0; i < RAND_NUMS; ++i ){
-                sum_a += array[ i ];
-        }
-        // printf("sum_a: %d\n", sum_a);
-        return sum_a;
+	for ( i = 0; i < RAND_NUMS; ++i ){
+		sum_simplest += array[ i ];
+	}
+	// printf("sum_simplest: %d\n", sum_simplest);
+	return sum_simplest;
 }
 
 /* The method which reduces the brances to do sum array */
 int sum_array_reduces_brances(int * array)
 {
-        int i;
-        for ( i = 0; i < RAND_NUMS; i += 20 ) {
-                sum_b += array[ i ];
-                sum_b += array[ i + 1 ];
-                sum_b += array[ i + 2 ];
-                sum_b += array[ i + 3 ];
-                sum_b += array[ i + 4 ];
-                sum_b += array[ i + 5 ];
-                sum_b += array[ i + 6 ];
-                sum_b += array[ i + 7 ];
-                sum_b += array[ i + 8 ];
-                sum_b += array[ i + 9 ];
-                sum_b += array[ i + 10];
-                sum_b += array[ i + 11 ];
-                sum_b += array[ i + 12 ];
-                sum_b += array[ i + 13 ];
-                sum_b += array[ i + 14 ];
-                sum_b += array[ i + 15 ];
-                sum_b += array[ i + 16 ];
-                sum_b += array[ i + 17 ];
-                sum_b += array[ i + 18 ];
-                sum_b += array[ i + 19 ];
-        }
-        // printf("sum_b: %d\n", sum_b);
-        return sum_b;
+	int i;
+	for ( i = 0; i < RAND_NUMS; i += 10 ) {
+		sum_reduces_brances += array[ i ];
+		sum_reduces_brances += array[ i + 1 ];
+		sum_reduces_brances += array[ i + 2 ];
+		sum_reduces_brances += array[ i + 3 ];
+		sum_reduces_brances += array[ i + 4 ];
+		sum_reduces_brances += array[ i + 5 ];
+		sum_reduces_brances += array[ i + 6 ];
+		sum_reduces_brances += array[ i + 7 ];
+		sum_reduces_brances += array[ i + 8 ];
+		sum_reduces_brances += array[ i + 9 ];
+	}
+
+	// for ( i = 0; i < RAND_NUMS; i += 20 ) {
+	// 	sum_reduces_brances += array[ i ];
+	// 	sum_reduces_brances += array[ i + 1 ];
+	// 	sum_reduces_brances += array[ i + 2 ];
+	// 	sum_reduces_brances += array[ i + 3 ];
+	// 	sum_reduces_brances += array[ i + 4 ];
+	// 	sum_reduces_brances += array[ i + 5 ];
+	// 	sum_reduces_brances += array[ i + 6 ];
+	// 	sum_reduces_brances += array[ i + 7 ];
+	// 	sum_reduces_brances += array[ i + 8 ];
+	// 	sum_reduces_brances += array[ i + 9 ];
+	// 	sum_reduces_brances += array[ i + 10];
+	// 	sum_reduces_brances += array[ i + 11 ];
+	// 	sum_reduces_brances += array[ i + 12 ];
+	// 	sum_reduces_brances += array[ i + 13 ];
+	// 	sum_reduces_brances += array[ i + 14 ];
+	// 	sum_reduces_brances += array[ i + 15 ];
+	// 	sum_reduces_brances += array[ i + 16 ];
+	// 	sum_reduces_brances += array[ i + 17 ];
+	// 	sum_reduces_brances += array[ i + 18 ];
+	// 	sum_reduces_brances += array[ i + 19 ];
+	// }
+	// printf("sum_reduces_brances: %d\n", sum_reduces_brances);
+	return sum_reduces_brances;
+}
+
+int sum_array_duffs_device(int * array)
+{
+	int i = 0;
+	int n = RAND_NUMS;
+
+	// switch ( n % 5 ) {
+	//
+	// 	case 0: do {sum_duffs_device += array[ i++ ];
+	// 	case 4:     sum_duffs_device += array[ i++ ];
+	// 	case 3:     sum_duffs_device += array[ i++ ];
+	// 	case 2:     sum_duffs_device += array[ i++ ];
+	// 	case 1:     sum_duffs_device += array[ i++ ];
+	//
+	// 	} while ( (n -= 5) > 0 );
+        // }
+	switch ( n % 10 ) {
+
+		case 0: do {sum_duffs_device += array[ i++ ];
+		case 9:     sum_duffs_device += array[ i++ ];
+		case 8:     sum_duffs_device += array[ i++ ];
+		case 7:     sum_duffs_device += array[ i++ ];
+		case 6:     sum_duffs_device += array[ i++ ];
+		case 5:     sum_duffs_device += array[ i++ ];
+		case 4:     sum_duffs_device += array[ i++ ];
+		case 3:     sum_duffs_device += array[ i++ ];
+		case 2:     sum_duffs_device += array[ i++ ];
+		case 1:     sum_duffs_device += array[ i++ ];
+
+		} while ( (n -= 10) > 0 );
+	}
+        // printf("sum_duffs_device: %d\n", sum_duffs_device);
+	return sum_duffs_device;
 }
 
 void check_two_array_sum(int * array)
 {
-        // run two differet method to sum array, and check the both sum are the same.
-        assert( sum_array_simplest(array) == sum_array_reduces_brances(array) );
+	// run differet methods to sum array, and check those sum are the same.
+	sum_simplest = sum_array_simplest(array);
+	sum_reduces_brances = sum_array_reduces_brances(array);
+	sum_duffs_device = sum_array_duffs_device(array);
 
-        // reset sum_a and sum_b
-        sum_a = 0;
-        sum_b = 0;
+	// assert(1);
+	// assert( sum_array_simplest == sum_array_reduces_brances );
+	// assert( sum_array_simplest == sum_array_duffs_device );
+
+	// reset sum_a and sum_b
+	sum_simplest = 0;
+	sum_reduces_brances = 0;
+	sum_duffs_device = 0;
 }
 
 
@@ -136,11 +206,12 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-        int * array = rn_arr_gen( RAND_NUMS, 0, 99 );
-        // print_arr( array, RAND_NUMS );
+	int * array = rn_arr_gen( RAND_NUMS, 0, 1000 );
+	// print_arr( array, RAND_NUMS );
 
-	for ( i = 0; i < RUN_TIMES; i++ ){
+	for ( i = 0; i < RUN_TIMES*10000; i++ ){
 		check_two_array_sum(array);
 	}
 
+	return 0;
 }
