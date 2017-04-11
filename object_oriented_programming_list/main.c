c013ac41df1836d4b925b92685b66cfc2ab35dbf
Lin Emsber <linemsber@gmail.com>
2017-04-11 23:51:51 +0800

    Test array list and add TODO
    1. Test array list in main.c
    2. Add TODO in generic.h
    	TODO: figure out the problem of container_of() from liunx/kernel.h

--- object_oriented_programming_list/main.c
+++ object_oriented_programming_list/main.c
@@ -12,21 +12,31 @@
 
 int main()
 {
-
         int i;
+
+        /* array list */
+        array_list_t * array_list_entry = array_list_create(0);
+
+	for(i = 0; i < LOOP_NUMS; ++i)
+		array_list_entry->list_op.push(&array_list_entry->list_op, i);
+
+	for(i = 0; i < LOOP_NUMS; ++i)
+		printf("%d ", array_list_entry->list_op.pop(&array_list_entry->list_op));
+
+	printf("is_empty: %d\n", array_list_entry->list_op.is_empty(&array_list_entry->list_op));
+
+	array_list_delete(array_list_entry);
+
+        /* linked list */
 	linked_list_t * linked_list_entry = linked_list_create();
 
 	for(i = 0; i < LOOP_NUMS; ++i)
-	{
 		linked_list_entry->list_op.push(&linked_list_entry->list_op, i);
-	}
 
 	for(i = 0; i < LOOP_NUMS; ++i)
-	{
 		printf("%d ", linked_list_entry->list_op.pop(&linked_list_entry->list_op));
-	}
-	printf("empty: %d\n", linked_list_entry->list_op.empty(&linked_list_entry->list_op));
-	printf("\n");
+
+	printf("is_empty: %d\n", linked_list_entry->list_op.is_empty(&linked_list_entry->list_op));
 
 	linked_list_delete(linked_list_entry);
 
