be654d2b1e11545339a5f799f02e2d12d92198c4
Lin Emsber <linemsber@gmail.com>
2017-04-12 23:12:25 +0800

    Fix typo

--- object_oriented_programming_list/generic.h
+++ object_oriented_programming_list/generic.h
@@ -3,14 +3,9 @@
 
 #define offsetof(TYPE, MEMBER) ( (size_t) &( (TYPE *)0 )-> MEMBER )
 
-/* Copy this macro: container_of() from linux/kernel.h, but it cannot work.
- * TODO: figure out the reason.
- */
-// #define container_of(ptr, type, member) ({                      \
-        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-        ((type *) ( (char*)(ptr) - offsetof(type, member)) ); })
+#define container_of(ptr, type, member) ({                          \
+        const typeof ( ((type *)0)->member ) *__mptr = (ptr);       \
+        (type *) ( (char *)__mptr - offsetof(type, member) ); })
 
-#define container_of(ptr, type, member) ({ \
-        (type *)( (char *) ptr - offsetof(type,member) );})
 
 #endif
