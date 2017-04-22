4ce10609b9eef64bdccacbd56a1b9065757cc6a0
Lin Emsber <linemsber@gmail.com>
2017-04-22 23:58:47 +0800

    Test linked queue

--- object_oriented_programming_list/main.c
+++ object_oriented_programming_list/main.c
@@ -10,44 +10,50 @@
 #include <unistd.h>
 
 #include "list.h"
+#include "queue.h"
 
 #define LOOP_NUMS 10
 
 int main()
 {
-        int i;
-        void * tmp_n;
+        // int i;
+        // void * tmp_n;
 
-        /* array list */
-        array_list_t * array_list_entry = array_list_create(0);
+        // /* array list */
+        // array_list_t * array_list_entry = array_list_create(0);
+        //
+	// for(i = 0; i < LOOP_NUMS; ++i)
+	// 	array_list_entry->list_op.push(&array_list_entry->list_op, i);
+        //
+	// for(i = 0; i < LOOP_NUMS; ++i){
+        //         tmp_n = array_list_entry->list_op.pop(&array_list_entry->list_op);
+        //         printf("tmp_n->value: %d\n", *(int *) tmp_n);
+        // }
+        //
+	// printf("is_empty: %d\n", array_list_entry->list_op.is_empty(&array_list_entry->list_op));
+        //
+	// array_list_delete(array_list_entry);
+        //
+        // /* linked list */
+	// linked_list_t * linked_list_entry = linked_list_create();
+        //
+	// for(i = 0; i < LOOP_NUMS; ++i)
+	// 	linked_list_entry->list_op.push(&linked_list_entry->list_op, i);
+        //
+	// for(i = 0; i < LOOP_NUMS; ++i){
+        //         tmp_n = linked_list_entry->list_op.pop(&linked_list_entry->list_op);
+        //         printf("tmp_n->value: %d\n", ( (node_t *) tmp_n )->value);
+        //         free(tmp_n);
+        // }
+        //
+	// printf("is_empty: %d\n", linked_list_entry->list_op.is_empty(&linked_list_entry->list_op));
+        //
+	// linked_list_delete(linked_list_entry);
 
-	for(i = 0; i < LOOP_NUMS; ++i)
-		array_list_entry->list_op.push(&array_list_entry->list_op, i);
 
-	for(i = 0; i < LOOP_NUMS; ++i){
-                tmp_n = array_list_entry->list_op.pop(&array_list_entry->list_op);
-                printf("tmp_n->value: %d\n", *(int *) tmp_n);
-        }
-
-	printf("is_empty: %d\n", array_list_entry->list_op.is_empty(&array_list_entry->list_op));
-
-	array_list_delete(array_list_entry);
-
-        /* linked list */
-	linked_list_t * linked_list_entry = linked_list_create();
-
-	for(i = 0; i < LOOP_NUMS; ++i)
-		linked_list_entry->list_op.push(&linked_list_entry->list_op, i);
-
-	for(i = 0; i < LOOP_NUMS; ++i){
-                tmp_n = linked_list_entry->list_op.pop(&linked_list_entry->list_op);
-                printf("tmp_n->value: %d\n", ( (node_t *) tmp_n )->value);
-                free(tmp_n);
-        }
-
-	printf("is_empty: %d\n", linked_list_entry->list_op.is_empty(&linked_list_entry->list_op));
-
-	linked_list_delete(linked_list_entry);
+        /* queue */
+        linked_queue_t * linked_queue_entry = linked_queue_create();
+        linked_queue_delete(linked_queue_entry);
 
 	return 0;
 }
