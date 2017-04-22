bf3dd1ece72a979f0310a59df96ae12d03203d1f
Lin Emsber <linemsber@gmail.com>
2017-04-22 23:56:56 +0800

    Add queue.h and linked_queue.c
    1. Add structures:
    	struct queue_operation
    	struct linked_queue
    2. Add functions:
    	linked_queue_operation_create()
    	linked_queue_create()
    	linked_queue_delete_queue()
    	linked_queue_delete()
    	linked_queue_add()
    	linked_queue_remove()
    	linked_queue_is_empty()

---
+++ object_oriented_programming_list/linked_queue.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "generic.h"
+#include "list.h"
+#include "queue.h"
+
+static void linked_queue_add(queue_operation_t * queue, int value);
+static node_t * linked_queue_remove(queue_operation_t * queue);
+static int linked_queue_is_empty(queue_operation_t * queue);
+
+linked_queue_t * linked_queue_operation_create(list_operation_t * _list)
+{
+	linked_queue_t * lq = malloc( sizeof(*lq) );
+	if(lq == NULL)
+		return NULL;
+
+	lq->list_op = _list;
+
+	lq->queue_op.add = linked_queue_add;
+	lq->queue_op.remove = (void *)linked_queue_remove;
+	lq->queue_op.is_empty = linked_queue_is_empty;
+
+	return lq;
+}
+
+linked_queue_t * linked_queue_create()
+{
+	linked_list_t * ll = linked_list_create();
+	if(ll == NULL)
+		return NULL;
+
+	return linked_queue_operation_create(&ll->list_op);
+}
+
+void linked_queue_delete_queue(linked_queue_t * _lq)
+{
+	free(_lq);
+}
+
+void linked_queue_delete(linked_queue_t *_lq)
+{
+	linked_list_t * ll = container_of(_lq->list_op, linked_list_t, list_op);
+	linked_list_delete(ll);
+	linked_queue_delete_queue(_lq);
+}
+
+
+/* operation */
+void linked_queue_add(queue_operation_t * _lq_op, int _value)
+{
+	linked_queue_t * lq = container_of(_lq_op, linked_queue_t, queue_op);
+	lq->list_op->push(lq->list_op, _value);
+}
+
+node_t * linked_queue_remove(queue_operation_t * _lq_op)
+{
+	linked_queue_t * lq = container_of(_lq_op, linked_queue_t, queue_op);
+	return lq->list_op->pop(lq->list_op);
+}
+
+int linked_queue_is_empty(queue_operation_t * _lq_op)
+{
+	linked_queue_t * lq = container_of(_lq_op, linked_queue_t, queue_op);
+	return lq->list_op->is_empty(lq->list_op);
+}
---
+++ object_oriented_programming_list/queue.h
@@ -0,0 +1,42 @@
+/* queue.h
+ * This header includes two kinds of queue: linked queue and blocking queue.
+ */
+
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "list.h"
+#include "generic.h"
+
+/* typedef */
+typedef struct queue_operation queue_operation_t;
+
+typedef struct linked_queue linked_queue_t;
+
+// typedef struct blocking_queue blocking_queue_t;
+
+
+/* structure */
+struct queue_operation{
+	void (* add)(struct queue_operation * queue_op, int value);
+	void * (* remove)(struct queue_operation * queue_op);
+	int (* is_empty)(struct queue_operation * queue_op);
+};
+
+struct linked_queue{
+	struct list_operation * list_op;
+	struct queue_operation queue_op;
+};
+
+
+extern linked_queue_t * linked_queue_operation_create(struct list_operation * list_op);
+extern linked_queue_t * linked_queue_create();
+extern void linked_queue_delete_queue(linked_queue_t * queue);
+extern void linked_queue_delete(linked_queue_t * queue);
+
+#endif
