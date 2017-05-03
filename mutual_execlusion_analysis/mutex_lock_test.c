af76542c8e203e0549254f6a15607ec46b77bc13
Lin Emsber <linemsber@gmail.com>
2017-05-03 23:43:22 +0800

    Add the implementation of Peterson's algorithm for mutual exclusion.
    1. Add Peterson's algorithm.
    2. Compare the both (Dekker's and Peterson's) mutex algorithm.
    3. Rename file:
            Dekker_lock.c -> mutex_lock_test.c

---
+++ mutual_execlusion_analysis/mutex_lock_test.c
@@ -0,0 +1,185 @@
+/* Dekker's algorithm Petersion's algorithm vs pthread_mutex_lock()
+ *
+ * USAGE: gcc -Wall -pthread -D mutex_lock_test.c && ./a.out
+ *
+ * MACRO:
+ *	_PTHREAD: use pthread_mutex_lock()
+ *	_D: use Dekker's algorithm to conduct lock.
+ *	_P: use Peterson's algorithm to conduct lock.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#include <pthread.h>
+
+
+#define BIG_NUM 500000000
+
+long  global_value = 0;
+
+int flag[2];
+int turn;
+
+#ifdef _PTHREAD
+pthread_mutex_t mutex;
+#endif
+
+/* functions of Dekker's lock */
+void d_lock_init()
+{
+	flag[0] = flag[1] = 0;
+	turn = 0;
+}
+
+
+void d_lock(int _self, int _other)
+{
+	flag[_self] = true;
+
+	while( flag[_other] == true ) {
+		if( turn != _self ) {
+			flag[_self] = false;
+			while( turn != _self );
+			flag[_self] = true;
+		}
+	}
+}
+
+void d_unlock(int _self, int _other)
+{
+	turn = _other;
+	flag[_self] = false;
+}
+
+
+
+/* functions of Peterson's lock */
+
+void p_lock_init()
+{
+	/* Initialize lock by reseting the desire of both the threads to acquire the locks.
+	 * And, giving turn to one of them.
+	 */
+	flag[0] = flag[1] = 0;
+	turn = 0;
+}
+
+void p_lock(int self)
+{
+	/* Set flag[self] = 1 saying you want to acquire lock. */
+	flag[self] = 1;
+
+	/* However, first give the other thread the chance to acquire lock. */
+	turn = 1-self;
+
+	/* Wait until the other thread looses the desirei to acquire lock,
+	 * or it is your turn to get the lock.
+	 */
+	while ( flag[ 1 - self ] == 1 && turn == 1 - self );
+}
+
+void p_unlock(int self)
+{
+	/* You do not desire to acquire lock in future.
+	 * This will allow the other thread to acquire the lock.
+	 */
+	flag[self] = 0;
+}
+
+
+
+void * calc(void * arg)
+{
+	long countdown = BIG_NUM;
+
+#ifdef _D
+	int self  = *(int*) arg;
+	int other = 1 - self;
+	d_lock(self, other);
+
+#elif _P
+	int self  = *(int*) arg;
+	p_lock(self);
+
+#elif _PTHREAD
+	pthread_mutex_lock(&mutex);
+#endif
+
+
+	/* critical section */
+	while( countdown > 0 ) {
+		countdown--;
+		global_value++;
+	}
+
+
+
+#ifdef _D
+	d_unlock(self, other);
+#elif _P
+	p_unlock(self);
+#elif _PTHREAD
+	pthread_mutex_unlock(&mutex);
+#endif
+
+	return 0;
+}
+
+double time_diff_in_second(struct timespec t1, struct timespec t2)
+{
+	struct timespec diff;
+
+	if (t2.tv_nsec - t1.tv_nsec < 0) {
+		diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+	} else {
+		diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+	}
+
+	return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
+int main()
+{
+	int * input_1;
+	int * input_2;
+        double time_diff;
+        struct timespec start, end;
+
+#ifdef _D
+	d_lock_init();
+#elif _P
+	p_lock_init();
+#elif _PTHREAD
+	pthread_mutex_init(&mutex, NULL);
+#endif
+
+	pthread_t thread[2];
+
+	input_1 = (int *) malloc( sizeof(int) );
+	input_2 = (int *) malloc( sizeof(int) );
+
+	* input_1 = 0;
+	* input_2 = 1;
+
+        clock_gettime(CLOCK_REALTIME, &start);
+
+	pthread_create(&thread[0], NULL, &calc, (void *) input_1);
+	pthread_create(&thread[1], NULL, &calc, (void *) input_2);
+
+	pthread_join(thread[0], NULL);
+	pthread_join(thread[1], NULL);
+
+	clock_gettime(CLOCK_REALTIME, &end);
+
+	printf("\tglobal_value: %lu\n", global_value);
+
+        time_diff = time_diff_in_second(start, end);
+        printf("\ttime difference in sencond: %lf\n", time_diff);
+
+	return 0;
+}
--- mutual_execlusion_analysis/run_test.sh
+++ mutual_execlusion_analysis/run_test.sh
@@ -1,34 +1,25 @@
 #/bin/bash
 
 echo "\n================================"
-echo "gcc -O0 with pthread_mutex_lock()"
-gcc -O0 -Wall -D_P -pthread Dekker_lock.c && ./a.out
-echo "gcc -O0 with Dekker's lock"
-gcc -O0 -Wall -D_D -pthread Dekker_lock.c && ./a.out
+echo "gcc -O0 with Peterson's algorithm"
+gcc -O0 -Wall -D_P -pthread mutex_lock_test.c && ./a.out
+echo "gcc -O0 with Dekker's algorithm"
+gcc -O0 -Wall -D_D -pthread mutex_lock_test.c && ./a.out
 
-echo "clang -O0 with pthread_mutex_lock()"
-clang -O0 -Wall -D_P -pthread Dekker_lock.c && ./a.out
-echo "clang -O0 with Dekker's lock"
-clang -O0 -Wall -D_D -pthread Dekker_lock.c && ./a.out
-
-echo "\n================================"
-echo "gcc -O3 with pthread_mutex_lock()"
-gcc -O3 -Wall -D_P -pthread Dekker_lock.c && ./a.out
-echo "gcc -O3  with Dekker's lock"
-gcc -O3 -Wall -D_D -pthread Dekker_lock.c && ./a.out
-
-echo "clang -O3 with pthread_mutex_lock()"
-clang -O3 -Wall -D_P -pthread Dekker_lock.c && ./a.out
-echo "clang -O3 with Dekker's lock"
-clang -O3 -Wall -D_D -pthread Dekker_lock.c && ./a.out
+echo "clang -O0 with Peterson's algorithm"
+clang -O0 -Wall -D_P -pthread mutex_lock_test.c && ./a.out
+echo "clang -O0 with Dekker's algorithm"
+clang -O0 -Wall -D_D -pthread mutex_lock_test.c && ./a.out
 
 echo "\n================================"
-echo "gcc -Ofast with pthread_mutex_lock()"
-gcc -Ofast -Wall -D_P -pthread Dekker_lock.c && ./a.out
-echo "gcc -Ofast with Dekker's lock"
-gcc -Ofast -Wall -D_D -pthread Dekker_lock.c && ./a.out
+echo "gcc with Peterson's algorithm"
+gcc -Ofast -Wall -D_P -pthread mutex_lock_test.c && ./a.out
+echo "gcc with Dekker's algorithm"
+gcc -Ofast -Wall -D_D -pthread mutex_lock_test.c && ./a.out
 
-echo "clang -Ofast with pthread_mutex_lock()"
-clang -Ofast -Wall -D_P -pthread Dekker_lock.c && ./a.out
-echo "clang -Ofast with Dekker's lock"
-clang -Ofast -Wall -D_D -pthread Dekker_lock.c && ./a.out
+echo "clang with Peterson's algorithm"
+clang -Ofast -Wall -D_P -pthread mutex_lock_test.c && ./a.out
+echo "clang with Dekker's algorithm"
+clang -Ofast -Wall -D_D -pthread mutex_lock_test.c && ./a.out
+
+echo "\n"
