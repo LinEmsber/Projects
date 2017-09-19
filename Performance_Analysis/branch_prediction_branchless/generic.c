95149bd6c43fab7854b8e942d10a54e2c35592d0
Lin Emsber <linemsber@gmail.com>
2017-09-19 12:48:37 +0800

    Complete the project
    1. Remove the test script.
    2. Separate the functions to different source code.
    3. Rename file: branchless.c -> main.c

--- Performance_Analysis/branch_prediction_branchless/generic.c
+++ Performance_Analysis/branch_prediction_branchless/generic.c
@@ -1,11 +1,3 @@
-/* branch prediction
- *
- * References:
- * http://stackoverflow.com/questions/11227809/why-is-it-faster-to-process-a-sorted-array-than-an-unsorted-array
- * https://www.collabora.com/news-and-blog/blog/2017/03/21/performance-analysis-in-linux/
- *
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -14,12 +6,11 @@
 #include <unistd.h>
 #include <time.h>
 
-
-int partition( int a[], int l, int r);
+#include "generic.h"
 
 void print_arr(int *arr, int len)
 {
-        int i;
+        unsigned int i;
 
         for ( i = 0; i < len; i++)
                 printf("%d ", *(arr + i) );
@@ -29,7 +20,7 @@ void print_arr(int *arr, int len)
 
 int * rand_num_generator_int( int len , int max, int min)
 {
-        int i;
+        unsigned int i;
         int * ret_arr = NULL;
         int range = max - min;
 
@@ -47,6 +38,13 @@ int * rand_num_generator_int( int len , int max, int min)
         return ret_arr;
 }
 
+int * array_copy(int * src, size_t size)
+{
+        int * dest = (int *) malloc (size * sizeof(int));
+        memcpy(dest, src, sizeof(int) * size);
+
+        return dest;
+}
 
 double time_diff_in_second(struct timespec t1, struct timespec t2)
 {
@@ -64,9 +62,9 @@ double time_diff_in_second(struct timespec t1, struct timespec t2)
 }
 
 
-void quick_sort( int a[], int l, int r)
+void quick_sort(int a[], int l, int r)
 {
-        int j;
+        unsigned int j;
 
         if( l < r )
         {
@@ -78,9 +76,9 @@ void quick_sort( int a[], int l, int r)
 
 }
 
-int partition( int a[], int l, int r)
+int partition(int a[], int l, int r)
 {
-        int pivot, i, j, t;
+        unsigned int pivot, i, j, t;
         pivot = a[l];
         i = l; j = r+1;
 
@@ -93,51 +91,4 @@ int partition( int a[], int l, int r)
         }
         t = a[l]; a[l] = a[j]; a[j] = t;
         return j;
-}
-
-int main()
-{
-	// array
-	int *array;
-	const int array_size = 327670;
-	long long sum = 0;
-
-	// time
-	struct timespec start, end;
-	double time_diff;
-
-	// array generate
-	array = rand_num_generator_int( array_size, 255, 0);
-//	print_arr(array, array_size);
-
-	// sorting
-#ifdef _SORT
-	quick_sort(array, 0, array_size - 1);
-#endif
-
-	// summation
-	clock_gettime(CLOCK_REALTIME, &start);
-	for (unsigned i = 0; i < 5000; ++i)
-	{
-		for (unsigned c = 0; c < array_size; ++c)
-		{
-
-#ifdef _NORMAL
-			if (array[c] >= 128)
-				sum += array[c];
-#elif _BITWISE
-			// int t = (array[c] - 128) >> 31;
-			sum += ~(array[c] - 128) >> 31 & array[c];
-#elif _CMOVGE
-			sum += (array[c] >= 128) ? array[c] : 0;
-#endif
-		}
-	}
-	clock_gettime(CLOCK_REALTIME, &end);
-
-	time_diff = time_diff_in_second(start, end);
-	printf("\ttime difference in second: %lf\n", time_diff);
-	printf("\tsum: %lld\n", sum);
-
-	return 0;
 }
